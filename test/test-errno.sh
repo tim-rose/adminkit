e7e53fd30b9b94388a87ba51abd1d800420040c6
Tim Rose <tim.rose@acm.org>
2017-06-13 14:16:57 +1000

    use PATH to find errno

--- test/test-errno.sh
+++ test/test-errno.sh
@@ -2,6 +2,7 @@
 #
 # test-errno --Tests for the errno command.
 #
+PATH=../src:$PATH
 MIDDEN_PATH=/usr/local/lib/sh
 . midden
 require tap
@@ -9,15 +10,15 @@ require test-more
 
 plan 5
 
-is "$(../src/errno 4)" \
+is "$(errno 4)" \
     '  4: EINTR       Interrupted system call' \
     "find error by number"
 
-is "$(../src/errno EINTR)" \
+is "$(errno EINTR)" \
     '  4: EINTR       Interrupted system call' \
     "find error by code"
 
-is "$(../src/errno 'system call')" \
+is "$(errno 'system call')" \
     '  4: EINTR       Interrupted system call' \
     "find error by (approximate) description"
 
@@ -26,12 +27,12 @@ expected="\
  23: ENFILE      Too many open files in system
  30: EROFS       Read-only file system"
 
-is "$(../src/errno 'system')" "$expected" \
+is "$(errno 'system')" "$expected" \
     "find by description can return multiple results"
 
 expected="\
   4: EINTR       Interrupted system call
  34: ERANGE      Result too large"
 
-is "$(../src/errno 4 34)" "$expected" \
+is "$(errno 4 34)" "$expected" \
     "can find by multiple arguments"
